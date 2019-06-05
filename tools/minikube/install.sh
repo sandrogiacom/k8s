ea2a9f4470d67d9b4a3a1d11ad737d81fac00784
Sandro <sandro.giacomozzi@fluig.com>
2019-06-05 18:59:56 -0300

    init commit

---
+++ tools/minikube/install.sh
@@ -0,0 +1,4 @@
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.1.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+sudo cp minikube /usr/local/bin/ && rm minikube
+minikube version
+pause
---
+++ tools/vbox6/install.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt upgrade
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
+sudo apt update
+sudo apt install virtualbox-6.0
+
