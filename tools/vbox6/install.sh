4d15fd833aa0478f1db8105dd5fbb5913c829103
Sandro <sandro.giacomozzi@fluig.com>
2019-06-18 03:01:02 -0300

    update vbox script

--- tools/vbox6/install.sh
+++ tools/vbox6/install.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt upgrade
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" >> /etc/apt/sources.list.d/virtualbox.list'
-sudo apt update && sudo apt install virtualbox-6.0
+sudo apt install virtualbox
