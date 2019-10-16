d8ab672d79b5b71ae5b59336c71dc028a70a8d6d
Sandro <sandro.giacomozzi@fluig.com>
2019-10-16 15:45:20 -0300

    Update minikube to 1.4.0

--- tools/minikube/install.sh
+++ tools/minikube/install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.2.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.4.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
