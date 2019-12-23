f2c9c9660e00ee2d6da02d08bbb0926089e41e09
Sandro Giacomozzi <sandrogiacom@gmail.com>
2019-12-23 11:16:57 -0300

    Update minikube script

--- tools/deb/minikube/install.sh
+++ tools/deb/minikube/install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.4.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/v1.6.2/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
